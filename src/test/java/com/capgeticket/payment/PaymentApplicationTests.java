72c1fa40cedbe78369c194c628120edc95d25007
Ranz Tabanera <alba.ranz-tabanera@capgemini.com>
2023-04-04 12:15:27 +0200

    initial commit

---
+++ src/test/java/com/capgeticket/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgeticket.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
