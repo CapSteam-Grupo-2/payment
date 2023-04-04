9a41dfd8507f25c3ef4192e0aeac2bc462917d26
Ranz Tabanera <alba.ranz-tabanera@capgemini.com>
2023-04-04 12:44:11 +0200

    add model and response

--- src/main/java/com/capgeticket/payment/models/Payment.java
+++ src/main/java/com/capgeticket/payment/models/Payment.java
@@ -1,4 +1,24 @@
 package com.capgeticket.payment.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
+
+    private String nombreTitular;
+    private String numeroTarjeta;
+    private String mesCaducidad;
+    private String yearCaducidad;
+    private String cvv;
+    private String emisor;
+    private String concepto;
+    private String cantidad;
 }
--- src/main/java/com/capgeticket/payment/responses/PaymentResponse.java
+++ src/main/java/com/capgeticket/payment/responses/PaymentResponse.java
@@ -1,4 +1,23 @@
 package com.capgeticket.payment.responses;
 
+import com.capgeticket.payment.models.Payment;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentResponse {
+    private String timestamp;
+    private String status;
+    private String error;
+    private List<String> message;
+    private Payment info;
+    private String infoadicional;
 }
