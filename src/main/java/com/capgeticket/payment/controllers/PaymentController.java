e8f125611f68a0c30e0fc7244a5dcb65a85614b5
Ranz Tabanera <alba.ranz-tabanera@capgemini.com>
2023-04-05 13:08:40 +0200

    add swagger

--- src/main/java/com/capgeticket/payment/controllers/PaymentController.java
+++ src/main/java/com/capgeticket/payment/controllers/PaymentController.java
@@ -3,6 +3,11 @@ package com.capgeticket.payment.controllers;
 import com.capgeticket.payment.models.Payment;
 import com.capgeticket.payment.responses.PaymentResponse;
 import com.capgeticket.payment.services.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +24,14 @@ public class PaymentController {
     @Autowired
     private PaymentService service;
 
+    @Operation(summary = "Realiza un pago", description = "Realiza un pago", tags = {"payment"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pago realizado correctamente", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PaymentResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Fallo en los datos", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PaymentResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "Servicio inestable", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PaymentResponse.class))})})
     @PostMapping("/new")
     public ResponseEntity<PaymentResponse> pay(@RequestBody Payment payment) {
         var result = service.pay(payment);
@@ -31,5 +44,3 @@ public class PaymentController {
         return ResponseEntity.internalServerError().body(result);
     }
 }
-
-
--- src/main/java/com/capgeticket/payment/error/GlobalExceptionHandler.java
+++ src/main/java/com/capgeticket/payment/error/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.capgeticket.payment.error;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
